ace0438d936637608abf58433a5e780b3d1ab8cc
BIKRAM GORAI <bikram77620@gmail.com>
2026-05-27 16:35:08 +0530

    fix: change apkUrl to raw.githubusercontent.com external link to force WebView system browser delegation and bump SW cache to v44

--- frontend/sw.js
+++ frontend/sw.js
@@ -23,7 +23,7 @@ messaging.onBackgroundMessage((payload) => {
   console.log('[SW] Background message received:', payload);
 });
 
-const CACHE_NAME = 'consistency-cache-v43';
+const CACHE_NAME = 'consistency-cache-v44';
 const STATIC_ASSETS = [
   '/',
   'index.html',
